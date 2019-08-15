93169754d17bb7f9484b1fbe52225d1b9f64c770
guillaume cromer <guillaumecromer@gmail.com>
2019-08-15 14:50:34 +0200

    Models, Controllers & View for Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+file = File.read('db/ingredients.json')
+json = JSON.parse(file)
+
+puts 'Creating seeds...'
+
+json["drinks"].each do |drinks|
+  drinks.each do |key, value|
+    Ingredient.create!(name: value)
+  end
+end
+
+puts 'Ingredients created!...'
