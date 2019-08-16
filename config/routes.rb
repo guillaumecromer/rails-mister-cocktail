0eb455fa9185dade361d8c1fd17a0a1844856b28
guillaume cromer <guillaumecromer@gmail.com>
2019-08-16 16:49:42 +0200

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'cocktails/new'
   # get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "cocktails#index"
+  root to: 'cocktails#index'
   resources :cocktails, only: [ :index, :show, :new, :create ] do
     resources :doses, only: [ :new, :create ]
   end
